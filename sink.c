6af0d48173f0ae23c30ad23bbc599f2b14cc15e2
JamieC1998 <jamie.cotter@mycit.ie>
2018-12-08 22:33:58 +0000

    Reintroduced Etimer

--- sink.c
+++ sink.c
@@ -51,7 +51,7 @@ PROCESS_THREAD(broadcast_process, ev, data){
     while(1) {
 
         /* Delay 4 seconds */
-        //etimer_set(&et, CLOCK_SECOND * 12 + random_rand() % (CLOCK_SECOND * 12));
+        etimer_set(&et, CLOCK_SECOND * 12 + random_rand() % (CLOCK_SECOND * 12));
 
         //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 

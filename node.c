8063c13a0cfc3aab62f26c0bd264cc9e2f9c80ae
JamieC1998 <jamie.cotter@mycit.ie>
2018-12-13 13:51:01 +0000

    Commented all my code

--- node.c
+++ node.c
@@ -11,15 +11,23 @@ Date        : 6/12/2018
 #include <stdio.h>
 
 /* These hold the broadcast and unicast structures, respectively. */
- struct broadcast_conn broadcast;
+struct broadcast_conn broadcast;
 
 
-
+/*
+Initialise my nodes
+sequence number and
+hopsFromSink
+*/
 static int sequenceNumber = -1;
 static int hopFromSink = -1;
 
+//This is used to store parent
 static linkaddr_t parent;
 
+//This is the struct
+//That will be received in
+//The broadcast
 struct message{
     int sequenceNumber;
     int hopCount;
@@ -27,16 +35,41 @@ struct message{
 
 };
 
+//Declaring my struct
 struct message status;
 
 PROCESS(broadcast_process, "Node process");
 
 AUTOSTART_PROCESSES(&broadcast_process);
 
+/*
+This function is called when a broadcast
+is received. It reads in the struct from
+the parent.
+If it's parent isn't set then it sets its
+parent to whoever sent the packet.
+It it's parent is already set and it
+receives a packet, it'll only read it 
+if it's from it's parent and the sequence
+number is greater than its own.
+Otherwise it'll only read if the wipe packet
+is set to 1, at which point it removes its
+set parent and erases its last recorded seq
+number
+*/
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
     struct message *message_pointer;
 
+    //Copying the struct in from the broadcast received
     message_pointer = packetbuf_dataptr();
+    /*
+    If the message pointer has its
+    wipe packet set to 1
+    we remove the current parent
+    we set the hopCount to 0 
+    and we set the Sequence number to 0
+    then we forward the packet 
+    */
     if(message_pointer -> wipe_value == 1){
         parent.u8[0] = NULL;
         parent.u8[1] = NULL;
@@ -49,7 +82,14 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
 
 
     }
+    
+    //Otherwise we fall in here
     else{
+
+        /*
+        If the hop count is 0 we fall in here
+        And set our parent and then forward the packet on
+        */
         if(message_pointer -> hopCount == 0){
             printf("Hop Count: %d\nSequence Number: %d\n", message_pointer->hopCount, message_pointer->sequenceNumber);
             status.sequenceNumber  = message_pointer->sequenceNumber;
@@ -62,6 +102,13 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
         
         }
 
+        /*
+        If the received sequence number
+        is greater than our own then we 
+        read in the packet
+        Set our parent to the packet received
+        And then we increase hop count and forward it
+        */
         else if(message_pointer->sequenceNumber > status.sequenceNumber || (parent.u8[0] == from->u8[0] &&  parent.u8[1] == from->u8[1])){
             printf("Hop Count: %d\nSequence Number: %d\n", message_pointer->hopCount, message_pointer->sequenceNumber);
 
@@ -75,6 +122,7 @@ static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
 
         }
     }
+    
     //packetbuf_copyfrom(&status, sizeof(struct message));
     //broadcast_send(&broadcast);
     //printf("Broadcast message sent from Node\n");
--- sink.c
+++ sink.c
@@ -10,6 +10,12 @@ Date        : 6/12/2018
 #include "random.h"
 #include <stdio.h>
 
+/*
+This struct is sent
+out to each node 
+so as to build the
+routing tree
+*/
 struct message {
     int sequenceNumber;
     int hopCount;
@@ -17,23 +23,40 @@ struct message {
 
 };
 
+//Starts the main process thread
 PROCESS(broadcast_process, "Broadcast process");
 AUTOSTART_PROCESSES(&broadcast_process);
 
+//This function is used for receiving the broadcast
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from){
     
 }
+
 
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 /* These hold the broadcast and unicast structures, respectively. */
 static struct broadcast_conn broadcast;
 
+/*
+This is the main program loop
+of the program, it sends out
+a broadcast every 4 - 8 seconds
+Every 20 broadcasts it sends out
+a wipe packet which wipes the 
+entire tree. After which it 
+resets the the sequence number
+to zero and starts again
+*/
 PROCESS_THREAD(broadcast_process, ev, data){
 
     //Creating our ETimer
     static struct etimer et;
+    
+    //This is the packet we will send
     struct message message_pointer;
 
+    //Our counter for the sequence
+    //number
     static int sequenceNumber = 0;
 
 
@@ -55,22 +78,34 @@ PROCESS_THREAD(broadcast_process, ev, data){
         etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
+        /*
+        In this block of code I set the 
+        hop count, the sequence number
+        and the wipe packet
+        */
         message_pointer.hopCount = 0;
         message_pointer.sequenceNumber = sequenceNumber;
         message_pointer.wipe_value = 0;
 
+        //If the sequence number
+        //Has passed 20 broadcasts
+        //We set the wipe packet 
+        //val to 1
         if(sequenceNumber > 20){
             message_pointer.wipe_value = 1;
         }
         /*
-        Every 4 seconds we copy a struct into the packetbuffer
+        Every 4 - 8 seconds we copy our struct into the packetbuffer
         and then send out the packet in a broadcast
         */
         packetbuf_copyfrom(&message_pointer, sizeof(struct message));
         broadcast_send(&broadcast);
 
+        //Then we increase the sequence number
         sequenceNumber += 1;
 
+        //If the wipe packet has been set
+        //We reset the sequence number
         if(message_pointer.wipe_value == 1){
             sequenceNumber = 0;
         }
